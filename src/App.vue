e00f28c7916ea5d3b05bb006368eb95ac8200e7f
a-merkin <arsen.merkin@gmail.com>
2025-06-09 16:50:40 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import ThreeMap from './components/ThreeMap.vue'
+</script>
+
+<template>
+  <ThreeMap 
+            :tableData="tables.tableCurrentGroupsWellData"
+            @changeSelectRow="changeSelectRow"
+            :selectedRows="selectedRows"
+        />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
